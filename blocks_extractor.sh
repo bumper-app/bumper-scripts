a4729ec3b136a4b16be43dfa5609f4b9c6ac2a62
mathieu <mathieu.nayrolles@gmail.com>
2015-04-26 13:17:09 -0400

    Magnify output for blocks extractor

--- blocks_extractor.sh
+++ blocks_extractor.sh
@@ -1,13 +1,15 @@
 #!/bin/bash 
 
 		#cd $1/$directory && nicad3 blocks $2 $sub $3 \
+increment = 0
 
 cd $1 && ls -d */ \
 | while read directory ; do  \
-	echo $directory 0; \
-	cd $1/$directory && nohup nicad3 blocks java 0 blindrenamereport > /dev/null \
-	echo $directory 1; \
-	cd $1/$directory && nohup nicad3 blocks java 1 blindrenamereport > /dev/null \
-	echo $directory 2; \
-	cd $1/$directory && nohup nicad3 blocks java 2 blindrenamereport > /dev/null \
+	(( increment++ )) \
+	&& echo -ne $increment $directory 0  \
+	&& cd $1/$directory && nicad3 blocks java 0 blindrenamereport &> /dev/null \
+	&& echo -ne 1  \
+	&& nicad3 blocks java 1 blindrenamereport &> /dev/null \
+	&& echo 2  \
+	&& nicad3 blocks java 2 blindrenamereport &> /dev/null \
 ; done
