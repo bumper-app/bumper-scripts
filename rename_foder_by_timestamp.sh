f3d97cbaaa608a6d37facf424c9a234810a28c36
mathieu <mathieu.nayrolles@gmail.com>
2015-04-27 21:23:13 -0400

    handle more than one diff file in directorty

---
+++ rename_foder_by_timestamp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+cd $1 && ls -d */ \
+| while read directory ; do 
+	count=`ls -l $1/$directory/diff_* | wc -l`
+	if [ $count -eq 1 ] 
+		then name=`head $1/$directory/diff_* -n 1 | awk -F ' ' '{print $1}'` \
+		&& mv $1/$directory $1/$name-$directory 
+	elif [ $count -ge 2 ]
+		then cat $1/$directory/diff_* >> $1/$directory/big_diff.txt && name=`head $1/$directory/big_diff.txt -n 1 | awk -F ' ' '{print $1}'` \
+		&& mv $1/$directory $1/$name-$directory 
+	fi \
+; done
